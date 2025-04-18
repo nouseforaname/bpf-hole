5e7b1ced5fb71213d2d56d30051599dd13c1e182
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2025-04-18 17:15:51 +0200

    this adds minimal parsing of the question section
    dns headers contain two byte ints ( that need to be bitshifted into
    available size ints) so for now we just rely on parsing some utf-8
    encoded bytes from the first section ( incidentially the data we want,
    the question section)...

--- bpf-hole-tc/src/main.rs
+++ bpf-hole-tc/src/main.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
 
+
 use aya_ebpf::{
-    bindings::{TC_ACT_PIPE, TC_ACT_SHOT},
-    macros::{classifier, map},
-    maps::HashMap,
-    programs::TcContext,
+    bindings::{TC_ACT_PIPE, TC_ACT_SHOT},macros::{classifier, map}, maps::HashMap, programs::TcContext
 };
 use aya_log_ebpf::info;
 use network_types::{
     eth::{EthHdr, EtherType},
-    ip::Ipv4Hdr,
+    ip::{IpProto, Ipv4Hdr}, udp::UdpHdr,
 };
 
 #[map]
@@ -24,10 +22,6 @@ pub fn bpf_hole_tc(ctx: TcContext) -> i32 {
     }
 }
 
-fn block_ip(address: u32) -> bool {
-    unsafe { BLOCKLIST.get(&address).is_some() }
-}
-
 fn try_bpf_hole_tc(ctx: TcContext) -> Result<i32, ()> {
     let ethhdr: EthHdr = ctx.load(0).map_err(|_| ())?;
     match ethhdr.ether_type {
@@ -38,15 +32,39 @@ fn try_bpf_hole_tc(ctx: TcContext) -> Result<i32, ()> {
     let ipv4hdr: Ipv4Hdr = ctx.load(EthHdr::LEN).map_err(|_| ())?;
     let destination = u32::from_be(ipv4hdr.dst_addr);
 
-    let action = if block_ip(destination) {
-        TC_ACT_SHOT
-    } else {
-        TC_ACT_PIPE
+    let (_, dst_port) = match ipv4hdr.proto {
+        IpProto::Tcp => return Ok(TC_ACT_PIPE),
+        IpProto::Udp => {
+            let udphdr: UdpHdr = ctx.load(EthHdr::LEN +Ipv4Hdr::LEN).map_err(|_| ())?;
+          (
+              u16::from_be(udphdr.source ),
+              u16::from_be(udphdr.dest ),
+           )
+        }
+        _ => {
+          return Ok(TC_ACT_PIPE);
+        },
     };
 
-    info!(&ctx, "DEST {:i}, ACTION {}", destination, action);
+    if dst_port != 53 {
+        return Ok(TC_ACT_PIPE);
+    }
+    let dns_header_len=12; // 96 bits for header data
+    let mut buf = [0; 256];
 
-    Ok(action)
+    info!(&ctx, "DNS Query to parsed: '{:i}:{}'", destination, dst_port);
+    let data_len= ctx.len();
+    let offset = EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN ;
+    info!(&ctx, "data_len: {}, offset: {} ", data_len,offset);
+    // unfortunately aya will generate code that won't pass the verifier:
+    // see: https://github.com/aya-rs/aya/pull/1218/files
+    // the use of check_bounds_signed is patched locally
+    // tried explicitly checking bounds here but failed.
+    ctx.load_bytes(offset + dns_header_len, &mut buf).map_err(|_| ())?;
+    let data = unsafe { core::str::from_utf8_unchecked(&buf) };
+    info!(&ctx, "data: {}", data);
+  //}
+    Ok(TC_ACT_PIPE)
 }
 
 #[cfg(not(test))]
