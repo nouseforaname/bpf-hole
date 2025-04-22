3e5bad16da97a8a644e7e1551d7a1c148135c32f
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2025-04-22 10:23:13 +0200

    feature: unpack network bytes into usable format
    use the dns header struct to parse the dns packets L7 header data.
    add loop on bytes to parse the bytes into a human readable string.
    
    - properly calc payload length.
    after the HEADER the packet contains the QNAME sections. This will
    contain the relevant data for making the decision whether we want to
    resolce a hostname or not. There are two u16 values at the end of the
    packet which should not be parsed into an utf-8 string.
    
    - dns QNAME bytes: will have a byte sequence with this format
    <u8: length of next segmen><ASCII CODE FOR first char of seqment>,...
    
    e.g.:
    `aaaa.bcd.cc`
    is represented by:
    
    [<4>, <97>,<97>,<97>,<97>, <3>, <98>,<99>,<100>, <2>,<99>,<99>]

--- bpf-hole-common/src/dns.rs
+++ bpf-hole-common/src/dns.rs
@@ -2,20 +2,22 @@
 pub struct DNSPacket {
     pub ID: u16, //2 byte
     pub HEADER_DATA: u16,
-  //pub QR: bool,
-  //pub OpCode: //4 bit int
-  //pub AA: bool,
-  //pub TC: bool,
-  //pub RD: bool,
-  //pub RA: bool,
-  //pub Z: // 3 static zero bits
-  //pub RCODE: //4 bit int
+  // QR: bit,
+  // OpCode: //4 bit int
+  // AA: bit,
+  // TC: bit,
+  // RD: bit,
+  // RA: bit,
+  // Z: // 3 static zero bits
+  // RCODE: //4 bit int
     pub QDCOUNT: u16, //2 byte
     pub ANCOUNT: u16, //2 byte
     pub NSCOUNT: u16, //2 byte
     pub ARCOUNT: u16, //2 byte
     //pub Payload: &'static[u8],
+
 }
  impl DNSPacket{
     pub const HDRLEN:usize = 12;
+    pub const DATA_SUFFIX_BYTES:usize = 4;
  }
--- bpf-hole-tc/src/main.rs
+++ bpf-hole-tc/src/main.rs
@@ -1,16 +1,16 @@
 #![no_std]
 #![no_main]
 
-
 use aya_ebpf::{
-    bindings::{TC_ACT_PIPE, TC_ACT_SHOT},macros::{classifier, map}, maps::HashMap, programs::TcContext
+    bindings::{TC_ACT_PIPE, TC_ACT_SHOT},macros::{classifier, map}, maps::{HashMap, PerCpuArray}, programs::TcContext
 };
 use aya_log_ebpf::info;
+use bpf_hole_common::dns::DNSPacket;
 use network_types::{
-    eth::{EthHdr, EtherType},
-    ip::{IpProto, Ipv4Hdr}, udp::UdpHdr,
+    eth::EthHdr,
+    ip::Ipv4Hdr, udp::UdpHdr,
 };
-
+const BUF_LEN:usize= 128;
 #[map]
 static BLOCKLIST: HashMap<u32, u32> = HashMap::with_max_entries(1024, 0);
 
@@ -23,23 +23,13 @@ pub fn bpf_hole_tc(ctx: TcContext) -> i32 {
 }
 
 fn try_bpf_hole_tc(ctx: TcContext) -> Result<i32, ()> {
-    let ethhdr: EthHdr = ctx.load(0).map_err(|_| ())?;
-    match ethhdr.ether_type {
-        EtherType::Ipv4 => {}
-        _ => return Ok(TC_ACT_PIPE),
-    }
 
     let ipv4hdr: Ipv4Hdr = ctx.load(EthHdr::LEN).map_err(|_| ())?;
-    let destination = u32::from_be(ipv4hdr.dst_addr);
-
-    let (_, dst_port) = match ipv4hdr.proto {
-        IpProto::Tcp => return Ok(TC_ACT_PIPE),
-        IpProto::Udp => {
+    let dst_port = match ipv4hdr.proto {
+        network_types::ip::IpProto::Tcp => return Ok(TC_ACT_PIPE),
+        network_types::ip::IpProto::Udp => {
             let udphdr: UdpHdr = ctx.load(EthHdr::LEN +Ipv4Hdr::LEN).map_err(|_| ())?;
-          (
-              u16::from_be(udphdr.source ),
-              u16::from_be(udphdr.dest ),
-           )
+            u16::from_be(udphdr.dest)
         }
         _ => {
           return Ok(TC_ACT_PIPE);
@@ -49,21 +39,46 @@ fn try_bpf_hole_tc(ctx: TcContext) -> Result<i32, ()> {
     if dst_port != 53 {
         return Ok(TC_ACT_PIPE);
     }
-    let dns_header_len=12; // 96 bits for header data
-    let mut buf = [0; 256];
 
-    info!(&ctx, "DNS Query to parsed: '{:i}:{}'", destination, dst_port);
-    let data_len= ctx.len();
-    let offset = EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN ;
-    info!(&ctx, "data_len: {}, offset: {} ", data_len,offset);
+    let offset = EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN;
+    let dns_packet:DNSPacket = ctx.load(offset).map_err(|_| ())?;
+    info!(&ctx, "ID: {}, QDCOUNT: {}, ANCOUNT: {}, {}, {}",
+        dns_packet.ID.to_be(),
+        dns_packet.QDCOUNT.to_be(),
+        dns_packet.ANCOUNT.to_be(),
+        dns_packet.NSCOUNT.to_be(),
+        dns_packet.ARCOUNT.to_be()
+    );
+
     // unfortunately aya will generate code that won't pass the verifier:
     // see: https://github.com/aya-rs/aya/pull/1218/files
     // the use of check_bounds_signed is patched locally
     // tried explicitly checking bounds here but failed.
-    ctx.load_bytes(offset + dns_header_len, &mut buf).map_err(|_| ())?;
-    let data = unsafe { core::str::from_utf8_unchecked(&buf) };
-    info!(&ctx, "data: {}", data);
-  //}
+
+    let offset = EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN + DNSPacket::HDRLEN;
+    let mut buf = [0u8;BUF_LEN];
+    ctx.load_bytes(offset, &mut buf).map_err(|_| {
+        info!(&ctx, "load bytes error");
+        ()
+    })?;
+    let mut bytes_until_segment_ends = buf[0];
+    let mut str_len=0;
+    for e in buf[1..].iter_mut(){
+        str_len += 1;
+        if bytes_until_segment_ends > 0 {
+            bytes_until_segment_ends -=1;
+            continue;
+        }
+        // the TLD . (after the TLD) marks the end of the hostname. QNAME uses an empty octet -> 0b00
+        if *e == 0 {
+            break;
+        }
+        bytes_until_segment_ends = *e;
+        *e = 46;
+    }
+    let data: &str = unsafe { str::from_utf8_unchecked(&buf[1..str_len.min(BUF_LEN)])};
+    info!(&ctx, "data: {} ,max index: {}",data,  str_len);
+
     Ok(TC_ACT_PIPE)
 }
 
