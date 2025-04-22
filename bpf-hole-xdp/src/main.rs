b0dae1f600ef94635462c8710376b0d41b2b566b
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2025-04-22 10:20:41 +0200

    remove experiments
    this codes was present to familiarize myself with the aya rs api for
    xpd. it is currently not required

--- bpf-hole-xdp/src/main.rs
+++ bpf-hole-xdp/src/main.rs
@@ -2,20 +2,12 @@
 #![no_main]
 
 use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
-use aya_log_ebpf::info;
 use bpf_hole_common::ptr_at;
 use network_types::{
     eth::{EthHdr, EtherType},
     ip::{IpProto, Ipv4Hdr},
-    //    tcp::TcpHdr,
     udp::UdpHdr,
 };
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
 
 #[xdp]
 pub fn bpf_hole_xdp(ctx: XdpContext) -> u32 {
@@ -39,7 +31,7 @@ fn try_bpf_hole(ctx: XdpContext) -> Result<u32, ()> {
         IpProto::Udp => {
             let udphdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
             (
-                u16::from_be(unsafe { (*udphdr).source }),
+u16::from_be(unsafe { (*udphdr).source }),
                 u16::from_be(unsafe { (*udphdr).dest }),
             )
         }
@@ -48,22 +40,12 @@ fn try_bpf_hole(ctx: XdpContext) -> Result<u32, ()> {
 
     let src_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
     let dst_addr = u32::from_be(unsafe { (*ipv4hdr).dst_addr });
-    // throw away everything that looks like a broadcast // multicast
-    // 4294967295 == 255.255.255.255
-    let mut octets_buf = [0u8; 16]; //initialize with whitespace => `32` == ` `
-
-    if dst_port == 5353 || src_addr == 4294967295 || octets_buf[3] == 255 {
-        return Ok(xdp_action::XDP_PASS);
-    }
-    let ip_str = bpf_hole_common::ip_str_from_u32(dst_addr, &mut octets_buf);
-    // do not care about multicast
-
-    info!(&ctx, "parsed: '{}'", ip_str);
-
-    info!(
-        &ctx,
-        "packet from: {:i}:{} -> {:i}:{}", src_addr, src_port, dst_addr, dst_port
-    );
 
     Ok(xdp_action::XDP_PASS)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
